43ab244dfa074b97d826927b85f7860c68cbfcfb
Josiah Yeow <josiah@propps.com>
2024-02-16 08:39:27 +1100

    feat: add routes for each statistic

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = new Koa()
 
 const router = new Router()
 
-router.get('/get-statistics', async (ctx) => {
+router.get('/all-statistics', async (ctx) => {
   const { logFileUrl } = ctx.query
 
   if (!logFileUrl) {
@@ -30,13 +30,69 @@ router.get('/get-statistics', async (ctx) => {
     countHttpRequestsByField(httpRequests, 'ipAddress')
   )
 
-  const result = {
+  ctx.body = {
     unique_ip_address_count: uniqueIpAddressCount,
     top_3_most_visited_urls: top3MostVisitedUrls,
     top_3_most_active_ip_addresses: top3MostActiveIpAddresses,
   }
+})
 
-  ctx.body = result
+router.get('/unique-ips', async (ctx) => {
+  const { logFileUrl } = ctx.query
+
+  if (!logFileUrl) {
+    ctx.status = 400
+    ctx.body = 'logFileUrl query parameter is required'
+    return
+  }
+
+  const httpRequests = await readLogFile(logFileUrl as string)
+
+  const uniqueIpAddressCount = countUniqueIpAddresses(httpRequests)
+
+  ctx.body = {
+    unique_ip_address_count: uniqueIpAddressCount,
+  }
+})
+
+router.get('/top-3-most-visited-urls', async (ctx) => {
+  const { logFileUrl } = ctx.query
+
+  if (!logFileUrl) {
+    ctx.status = 400
+    ctx.body = 'logFileUrl query parameter is required'
+    return
+  }
+
+  const httpRequests = await readLogFile(logFileUrl as string)
+
+  const top3MostVisitedUrls = getTop3(
+    countHttpRequestsByField(httpRequests, 'resource')
+  )
+
+  ctx.body = {
+    top_3_most_visited_urls: top3MostVisitedUrls,
+  }
+})
+
+router.get('/top-3-most-active-ips', async (ctx) => {
+  const { logFileUrl } = ctx.query
+
+  if (!logFileUrl) {
+    ctx.status = 400
+    ctx.body = 'logFileUrl query parameter is required'
+    return
+  }
+
+  const httpRequests = await readLogFile(logFileUrl as string)
+
+  const top3MostActiveIpAddresses = getTop3(
+    countHttpRequestsByField(httpRequests, 'ipAddress')
+  )
+
+  ctx.body = {
+    top_3_most_active_ip_addresses: top3MostActiveIpAddresses,
+  }
 })
 
 app.use(router.routes()).use(router.allowedMethods())
